13bf33ea1d54288c918d759bf652cbb718bf8555
Joshua Kurz <josh.kurz@weightwatchers.com>
2020-09-09 11:28:54 -0400

    init commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Docker Exercise
+The objective of this repo is to show that you can debug a very simple application, dockerize that application, and create a dockerfile for it.
+
+## Step 1
+Fix the broken app
+
+# Step 2
+Create a Docker file for the app
+
+# Step 3
+Create a docker-compose file for the app.
+
+# Step 4
+docker-compose up
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const hc = process.env.HEALTHCHECK || '/hc'
+const app = express()
+
+app.use(bodyParser.text({type: "*/*", limit: '50mb'}))
+app.get(hc, (req, res) => res.send('OK'))
+app.use(function(req, res, next) { // i catch 404s
+    res.status(404).send("404")
+})
+app.use((err, req, res, next) => {
+    console.log('ERROR =>', err)
+    res.status(500).send("500")
+})
+app.listen(PORT, () => console.log(`logger app listening on port xxx!`))
