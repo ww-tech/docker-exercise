5da3dc4de1779a96c0696ff4ec64a9650357525d
sami007 <student-00-98bc64b12fbe@qwiklabs.net>
2020-09-28 20:39:02 -0400

    add port to listen

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const hc = process.env.HEALTHCHECK || '/hc'
 const app = express()
+const PORT = 8080
 
 app.use(bodyParser.text({type: "*/*", limit: '50mb'}))
 app.get(hc, (req, res) => res.send('OK'))
@@ -12,4 +13,4 @@ app.use((err, req, res, next) => {
     console.log('ERROR =>', err)
     res.status(500).send("500")
 })
-app.listen(PORT, () => console.log(`logger app listening on port xxx!`))
+app.listen(PORT, () => console.log("logger app listening on port "+PORT+"!"))
